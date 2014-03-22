4e9324df3f165d422abd306d060eb659fb17449b
Daniel van Dorp <daniel@vandorp.biz>
2014-03-22 17:01:24 +0100

    publishconf updated

--- publishconf.py
+++ publishconf.py
@@ -11,14 +11,17 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://vandorp.biz'
-RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+SOCIAL = SOCIAL + (('rss', SITEURL + '/' + FEED_ALL_ATOM),)
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = 'dandydev'
+# GOOGLE_ANALYTICS = 'UA-24099413-2'
+# ADDTHIS_PROFILE = 'ra-4f274b9c7023574d'
+
+RELATIVE_URLS = False
